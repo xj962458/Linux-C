a0dd95cdbb0179b8b946db490e3fec8ae9a3df3b
xj962458 <2291921855@qq.com>
2020-11-03 17:02:14 +0800

    xj2020-11-03

--- VScode-Python/作业/Test.py
+++ VScode-Python/作业/Test.py
@@ -1,7 +1,6 @@
-# 画矩形
-import turtle as t
-t.setup(500,500,500,500)
-for i in range(4):
-    t.fd(200)
-    t.left(90)
-t.done()
+def Trans(f):
+    return (f-32)*5/9
+def main():
+    f = eval(input("请输入一个华氏温度:\n"))
+    print("{:.2f}℉转换为摄氏温度是{:.2f}℃".format(f, Trans(f)))
+main()
